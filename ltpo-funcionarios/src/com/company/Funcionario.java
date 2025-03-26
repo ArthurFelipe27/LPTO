49724ef1f85dcf0cfabd26c4bef0ac8b146ed40a
ArthurFelipe27 <arthurfelipematos837@gmail.com>
2025-03-26 10:16:47 -0300

    Modifiquei as LTPO Funcionarios

---
+++ ltpo-funcionarios/src/com/company/Funcionario.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Funcionario {
+    private String nome;
+    private double salarioBase;
+    private int horasTrabalhadas;
+
+    public Funcionario(String nome, double salarioBase, int horasTrabalhadas) {
+        this.nome = nome;
+        this.salarioBase = salarioBase;
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    public  double calcularSalarioFinal() {
+        int horasNormais = 160;
+        double valorHora = salarioBase / horasNormais;
+        if (horasTrabalhadas > horasNormais) {
+            int horasExtras = horasTrabalhadas - horasNormais;
+            return salarioBase + (horasExtras * valorHora * 1.5);
+        }
+        return salarioBase;
+    }
+
+    public void exibirDados(){
+        System.out.printf("Funcionarios: %s\n", nome);
+        System.out.printf("Salário Base: R$%.2f\n", salarioBase);
+        System.out.printf("Horas trabalhadas: %d\n", horasTrabalhadas);
+        System.out.printf("Salário Final: R$%.2f\n", calcularSalarioFinal());
+        System.out.println("----------------------------------------------");
+    }
+}
--- ltpo-funcionarios/src/com/company/Main.java
+++ ltpo-funcionarios/src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner exibir = new Scanner(System.in);
+    Funcionario[] funcionarios = {
+            new Funcionario("Luana Pereira", 3000.0, 170),
+            new Funcionario("Arthur Felipe", 2800.0, 160),
+            new Funcionario("Lourdes Felipe", 3500.0, 180),
+            new Funcionario("Antonio Cesar", 2500.0, 155)
+    };
+
+    System.out.println("\nRelatório de Funcionários:");
+    for (Funcionario funcionario : funcionarios){
+        funcionario.exibirDados();
+    }
+
     }
 }
