c313928cdcefab496dfa14e706b8cc65d815d916
Arthur Felipe <arthurfelipematos837@gmail.com>
2025-04-02 09:37:35 -0300

    Add files via upload

---
+++ ltpo/src/main/java/com/fabricadebolo/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.fabricadebolo;
+
+import main.java.com.fabricadebolo.model.Bolo;
+
+public class Main {
+    public static void main(String[] args) {
+        Bolo bolo = new Bolo("Chocolate", 20);
+        bolo.exibirDetalhes();
+    }
+}
